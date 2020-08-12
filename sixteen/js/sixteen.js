094817a63eb48636758a87ce51cef024b687fdbc
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-08-12 15:33:44 -0700

    First commit

---
+++ sixteen/js/sixteen.js
@@ -0,0 +1,26 @@
+window.addEventListener("load", (event) => {
+  addButtonListener();
+});
+
+const addButtonListener = () => {
+  const navBtns = document.getElementsByClassName("nav-buttons");
+  for (let item of navBtns) {
+    item.addEventListener("click", () => {
+      resetBtnBorder();
+      const btn = event.currentTarget;
+      setBtnBorder(btn);
+    });
+  }
+};
+
+const resetBtnBorder = () => {
+  const navBtns = document.getElementsByClassName("nav-buttons-clicked");
+  if (navBtns !== null) {
+    for (let item of navBtns) {
+      item.className = "nav-buttons";
+    }
+  }
+};
+const setBtnBorder = (btn) => {
+  btn.className = "nav-buttons-clicked";
+};
