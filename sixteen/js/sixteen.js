833b06d25f616ee74d37ed07bb6bfe0cdede3328
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-08-13 16:15:52 -0700

    Added specialty carousel.

--- sixteen/js/sixteen.js
+++ sixteen/js/sixteen.js
@@ -31,7 +31,11 @@ const addArrowListener = () => {
   const arrowBtns = document.getElementsByClassName("arrow-div");
   for (let item of arrowBtns) {
     item.addEventListener("click", () => {
-      console.log(event.currentTarget);
+      if (event.currentTarget.id === "left-arrow") {
+        plusSlides(-1);
+      } else {
+        plusSlides(1);
+      }
     });
   }
 };
@@ -42,3 +46,45 @@ const changeHomeScreen = () => {
 };
 
 // Carousel
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("specialty-slides");
+  let dots = document.getElementsByClassName("dot");
+  const defaultSpecialty = document.getElementById("specialty-default");
+  defaultSpecialty.style.display = "none";
+  const defaultDot = document.getElementById("default-dot");
+  if (defaultDot !== null) {
+    defaultDot.id = "";
+  }
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  console.log(slides[slideIndex - 1] !== "undefined");
+  if (slides[slideIndex - 1] !== "undefined") {
+    slides[slideIndex - 1].style.display = "block";
+  }
+
+  dots[slideIndex - 1].className += " active";
+}
