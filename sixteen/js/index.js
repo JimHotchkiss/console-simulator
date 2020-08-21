3ce5a0215936c1fd3300c347e30d392a6fa89e48
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-08-21 12:46:57 -0700

    Adding in the basic UI for the Crossfire simulator.

--- sixteen/js/index.js
+++ sixteen/js/index.js
@@ -4,27 +4,19 @@ window.addEventListener("load", (event) => {
 
 const selectConsoleListener = () => {
   const consoles = document.getElementsByClassName("console");
-  const sixteen = document.getElementById("1688-console");
-  const crossfire = document.getElementById("crossfire-console");
   for (let item of consoles) {
     item.addEventListener("click", () => {
+      const currentConsole = document.getElementById(item.id + "-console");
       resetDisplayConsole();
-      if (item.id === "crossfire") {
-        crossfire.classList.add("show");
-      } else if (item.id === "1688") {
-        console.log(sixteen);
-        sixteen.classList.add("show");
-      }
+      currentConsole.classList.add("show");
     });
   }
 };
 
 const resetDisplayConsole = () => {
-  console.log("here");
   const show = document.getElementsByClassName("show");
   if (show.length > 0) {
     for (let item of show) {
-      console.log(show);
       item.classList.remove("show");
     }
   }
