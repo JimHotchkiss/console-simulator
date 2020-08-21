b0f8538c6f57d8a0a0e52eb30a60dacae636aa92
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-08-20 18:29:03 -0700

    Update.

--- sixteen/js/index.js
+++ sixteen/js/index.js
@@ -6,6 +6,6 @@ const selectConsoleListener = () => {
   const roundImgDiv = document.getElementById("round-img-div");
   roundImgDiv.addEventListener("click", () => {
     const simulator = document.getElementById("simulator");
-    simulator.classList.add("show");
+    simulator.classList.toggle("show");
   });
 };
