6765776169f40912912c707e9ee0aa69291caedb
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-08-21 09:51:23 -0700

    Adding the UI for the Crossfire console.

--- sixteen/js/index.js
+++ sixteen/js/index.js
@@ -3,9 +3,29 @@ window.addEventListener("load", (event) => {
 });
 
 const selectConsoleListener = () => {
-  const roundImgDiv = document.getElementById("1688");
-  roundImgDiv.addEventListener("click", () => {
-    const simulator = document.getElementById("simulator");
-    simulator.classList.toggle("show");
-  });
+  const consoles = document.getElementsByClassName("console");
+  const sixteen = document.getElementById("1688-console");
+  const crossfire = document.getElementById("crossfire-console");
+  for (let item of consoles) {
+    item.addEventListener("click", () => {
+      resetDisplayConsole();
+      if (item.id === "crossfire") {
+        crossfire.classList.add("show");
+      } else if (item.id === "1688") {
+        console.log(sixteen);
+        sixteen.classList.add("show");
+      }
+    });
+  }
+};
+
+const resetDisplayConsole = () => {
+  console.log("here");
+  const show = document.getElementsByClassName("show");
+  if (show.length > 0) {
+    for (let item of show) {
+      console.log(show);
+      item.classList.remove("show");
+    }
+  }
 };
