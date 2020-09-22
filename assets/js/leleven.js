e0d1f03c1ab2a7e9bc03497f0d5cd19c3348e4a9
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-09-22 16:42:01 -0700

    Added the iris div.

--- assets/js/leleven.js
+++ assets/js/leleven.js
@@ -5,6 +5,14 @@ window.addEventListener("load", (event) => {
 const handlePowerButton = () => {
   const powerButton = document.getElementById("power-button");
   powerButton.addEventListener("click", () => {
+    testIris();
     powerButton.classList.toggle("on");
   });
+};
+
+const testIris = () => {
+  const irisDiv = document.getElementsByClassName("iris-plug-div");
+  for (let item of irisDiv) {
+    item.classList.toggle("on");
+  }
 };
