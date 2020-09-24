5c44d4b47ebae01c99644fa1c4de5b354ff51f6d
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-09-24 14:58:35 -0700

    Created light source toggle and light cord.

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,16 +6,24 @@ const selectConsoleListener = () => {
   const consoles = document.getElementsByClassName("console");
   for (let item of consoles) {
     item.addEventListener("click", () => {
-      console.log(item.id);
-      const currentConsole = document.getElementById(item.id + "-console");
-      console.log(currentConsole);
-      resetDisplayConsole();
-      currentConsole.classList.add("show");
+      if (item.id === "l11") {
+        const lightCableDiv = document.getElementById("lightcable-div");
+        resetDisplayConsole();
+        lightCableDiv.classList.add("show");
+        const currentConsole = document.getElementById(item.id + "-console");
+        currentConsole.classList.add("show");
+      } else {
+        const currentConsole = document.getElementById(item.id + "-console");
+        resetDisplayConsole();
+        currentConsole.classList.add("show");
+      }
     });
   }
 };
 
 const resetDisplayConsole = () => {
+  const lightCableDiv = document.getElementById("lightcable-div");
+  lightCableDiv.classList.remove("show");
   const show = document.getElementsByClassName("show");
   if (show.length > 0) {
     for (let item of show) {
--- assets/js/leleven.js
+++ assets/js/leleven.js
@@ -2,7 +2,46 @@ window.addEventListener("load", (event) => {
   handlePowerButton();
   handleNavButton();
   setHomeIconSelected();
+  handleLightCable();
+  handleLightSourceToggle();
 });
+
+const handleLightSourceToggle = () => {
+  const powerBtn = document.getElementById("powerBtn-div");
+  powerBtn.addEventListener("click", () => {
+    if (checkLightCordPlug() === true) {
+      console.log(checkLightCordPlug());
+      powerBtn.classList.toggle("powerBtn-div-on");
+    } else {
+      console.log(checkLightCordPlug());
+      alert("Remeber to connect a lightcable");
+    }
+  });
+};
+
+const checkLightCordPlug = () => {
+  const lightcordDiv = document.getElementById("lightcord-div");
+  if (lightcordDiv.classList.value === "lightcord-div light-on") return true;
+};
+
+const handleLightCable = () => {
+  const powerBtn = document.getElementById("power-button");
+
+  const lightCableDiv = document.getElementById("lightcable-div");
+  lightCableDiv.addEventListener("click", () => {
+    console.log(powerBtn.classList.value);
+    if (powerBtn.classList.value !== "power-button-div on") {
+      console.log(powerBtn.classList.value);
+      alert("Be sure to power on the L11!");
+    } else {
+      const lightcableDiv = document.getElementById("lightcord-div");
+      lightcableDiv.classList.toggle("light-on");
+      const lightcableImg = document.getElementById("lightcord-img");
+      lightcableImg.classList.toggle("light-on-img");
+      lightToggle();
+    }
+  });
+};
 
 const setHomeIconSelected = () => {
   const l11HomeBtn = document.getElementById("l11-home-btn");
@@ -10,24 +49,22 @@ const setHomeIconSelected = () => {
 };
 
 const handlePowerButton = () => {
+  const powerBtnDiv = document.getElementById("powerBtn-div");
+  const lightcablediv = document.getElementById("lightcord-div");
   const powerButton = document.getElementById("power-button");
   const uiContainer = document.getElementById("ui-container");
   powerButton.addEventListener("click", () => {
-    // testIris();
-    // testLightToggle();
-
+    if (lightcablediv.classList.value === "lightcord-div light-on") {
+      lightcablediv.classList.toggle("light-on");
+      powerBtnDiv.classList.toggle("powerBtn-on");
+    }
     powerButton.classList.toggle("on");
     uiContainer.classList.toggle("power-on");
   });
 };
 
-const testLightToggle = () => {
-  const lightcableDiv = document.getElementById("lightcord-div");
-  lightcableDiv.classList.toggle("light-on");
-  const lightcableImg = document.getElementById("lightcord-img");
-  lightcableImg.classList.toggle("light-on-img");
+const lightToggle = () => {
   const powerBtnDiv = document.getElementById("powerBtn-div");
-  console.log(powerBtnDiv);
   powerBtnDiv.classList.toggle("powerBtn-on");
 };
 
